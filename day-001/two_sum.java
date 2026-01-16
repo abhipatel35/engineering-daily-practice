96427494e796d3dc6be4de03250b9f566721c1b3
Abhi Patel <abhipatel0035@gmail.com>
2026-01-15 23:21:37 -0500

    Day 001 – HashMap based solution (Java)

---
+++ day-001/two_sum.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+
+    // Brute force approach - O(n^2)
+    // Uncomment to understand the naive solution
+    /*
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    */
+
+    // Optimized approach: One-Pass HashMap (Complement Lookup) - O(n)
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[]{};
+    }
+}
